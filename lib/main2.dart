045d4aba7a647fccf0461f4a767a0a3aad076186
dkdkdkdk555 <dkdkdkdk555@naver.com>
2024-02-27 23:30:03 +0900

    #4 3 Recap

--- lib/main2.dart
+++ lib/main2.dart
@@ -20,11 +20,12 @@ class Apsp extends StatefulWidget {
 // 2. 데이터가 바뀌면 변하는 부분
 // UI를 구축하는 곳, 새로 고침시 최신 데이터를 보여줌
 class _AppState extends State<Apsp> {
-  int counter = 0; // 데이터 는 걍 클래스 property임
+  
+  List<int> numbers = [];
 
   void onClicked(){
     setState(() {  // UI에게 데이터가 바뀌었다고 알리는 함수
-      counter = counter + 1;
+      numbers.add(numbers.length);
     });
   }
 
@@ -41,9 +42,7 @@ class _AppState extends State<Apsp> {
                 'Click Count',
                 style: TextStyle(fontSize: 30),
               ),
-              Text('$counter',
-                style: const TextStyle(fontSize: 30),
-              ),
+              for(var n in numbers) Text('$n'),
               IconButton(
                 iconSize: 30,
                 onPressed: onClicked, 
