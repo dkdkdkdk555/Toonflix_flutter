3245a1573497ae31e99182bfceec13d38e8ffc41
dkdkdkdk555 <dkdkdkdk555@naver.com>
2024-02-27 23:56:32 +0900

    #4 5 Widget Lifecycle

--- lib/main2.dart
+++ lib/main2.dart
@@ -22,6 +22,14 @@ class Apsp extends StatefulWidget {
 class _AppState extends State<Apsp> {
   
 
+  bool showTitle = true;
+
+  void toggleTittle(){
+    setState(() {
+      showTitle = !showTitle;      
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -32,14 +40,14 @@ class _AppState extends State<Apsp> {
           ),
         ),
       ),
-      home: const Scaffold(
+      home: Scaffold(
         backgroundColor: Colors.white,
         body: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              MyLargeTitle(),
-          
+              showTitle ? const MyLargeTitle() : const Text('nothing'),
+              IconButton(onPressed: toggleTittle, icon: const Icon(Icons.remove_red_eye))
             ],
           ),
         ),
@@ -48,11 +56,36 @@ class _AppState extends State<Apsp> {
   }
 }
 
-class MyLargeTitle extends StatelessWidget {
+class MyLargeTitle extends StatefulWidget {
   const MyLargeTitle({
     super.key,
   });
 
+  @override
+  State<MyLargeTitle> createState() => _MyLargeTitleState();
+}
+
+class _MyLargeTitleState extends State<MyLargeTitle> {
+
+  @override
+  void initState() { // 상태 초기화
+    super.initState();
+    /*  
+      <initState가 사용되는 경우>
+      - 데이터 초기화 때 부모 요소에 의존하는 데이터를 초기화 해주는 경우
+      - context를 통해 부모요소에 접근해야 하는 경우
+      - API에서 업데이트를 구독해야 하는 경우
+    */ 
+  }
+
+  @override
+  void dispose() { // 위젯이 스크린에서 제거 될 때
+    super.dispose();
+    /*
+      - API 업데이터
+      - 이벤트 리스너로부터 구독 취소
+    */
+  }
   @override
   Widget build(BuildContext context) { 
   // BuildContext context 는 상위요소의 모든 정보를 가지고 있다. 
